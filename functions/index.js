eaab3d3a6e0cd9e5f957fe07b7f1b812edebbce5
Tom Fanella <fanella@purdue.edu>
2017-08-27 23:32:42 -0400

    Initial Commit

---
+++ functions/index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+const express = require('express');
+const cors = require('cors')({origin: true});
+const app = express();
+
+// Express middleware that validates Firebase ID Tokens passed in the Authorization HTTP header.
+// The Firebase ID token needs to be passed as a Bearer token in the Authorization HTTP header like this:
+// `Authorization: Bearer <Firebase ID Token>`.
+// when decoded successfully, the ID Token content will be added as `req.user`.
+// Note: __session support has been removed but may be added back in the future
+const validateFirebaseIdToken = (req, res, next) => {
+  console.log('Check if request is authorized with Firebase ID token');
+
+  if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
+    console.error('No Firebase ID token was passed as a Bearer token in the Authorization header.',
+        'Make sure you authorize your request by providing the following HTTP header:',
+        'Authorization: Bearer <Firebase ID Token>');
+    res.status(403).send('Unauthorized');
+    return;
+  }
+
+  console.log('Found "Authorization" header');
+  // Read the ID Token from the Authorization header.
+  let idToken = req.headers.authorization.split('Bearer ')[1];;
+  
+  admin.auth().verifyIdToken(idToken).then(decodedIdToken => {
+    console.log('ID Token correctly decoded', decodedIdToken);
+    req.user = decodedIdToken;
+    next();
+  }).catch(error => {
+    console.error('Error while verifying Firebase ID token:', error);
+    res.status(403).send('Unauthorized');
+  });
+};
+
+app.use(cors);
+app.use(validateFirebaseIdToken);
+app.get('/', (req, res) => {
+  res.send(`Welcome to the API ${req.user.name}`);
+});
+
+// This HTTPS endpoint can only be accessed by your Firebase Users.
+// Requests need to be authorized by providing an `Authorization` HTTP header
+// with value `Bearer <Firebase ID Token>`.
+exports.api = functions.https.onRequest(app);
+
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!" + functions.config().employability.key);
+// });
